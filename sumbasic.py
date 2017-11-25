da4b1cc51aeb4abf77094a24c6536cab6fb18293
Theodore Morley <theodore.morley@mail.mcgill.ca>
2017-11-25 15:27:08 -0500

    Initial commit

---
+++ sumbasic.py
@@ -0,0 +1,22 @@
+from nltk.stem.wordnet import WordNetLemmatizer
+
+'''
+Takes in a string (data), and applies standard preprocessing to it, returning a list of tokens.
+'''
+def preprocess(data):
+    return list(data)
+
+def orig():
+    return True
+
+def simplified():
+    return True
+
+def leading():
+    return True
+
+def main():
+    return True
+
+if __name__ == "__main__":
+    main()
